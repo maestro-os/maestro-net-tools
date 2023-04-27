b8c12072164b55724bfdd3ff3060896637521f74
llenotre <llenotre@student.42.fr>
2023-04-28 00:34:17 +0200

    cargo fmt

--- ping/src/main.rs
+++ ping/src/main.rs
@@ -11,78 +11,78 @@ use std::time::Duration;
 
 /// Structure storing arguments.
 struct Args {
-	/// The number of packets to send.
-	///
-	/// If `None`, there is no limit.
-	count: Option<NonZeroUsize>,
-	/// The interval between echo packets.
-	///
-	/// If `None`, a duration of 1 second is used.
-	interval: Option<Duration>,
-	/// The timeout before `ping` exits regardless of how many packets have been sent.
-	deadline: Option<Duration>,
-	/// The time to wait for a response for each packet.
-	///
-	/// If `None`, TODO
-	timeout: Option<Duration>,
-	/// The size of packets to be sent.
-	packet_size: Option<usize>,
-	/// IP Time To Live.
-	///
-	/// If `None`, TODO
-	ttl: Option<u32>,
+    /// The number of packets to send.
+    ///
+    /// If `None`, there is no limit.
+    count: Option<NonZeroUsize>,
+    /// The interval between echo packets.
+    ///
+    /// If `None`, a duration of 1 second is used.
+    interval: Option<Duration>,
+    /// The timeout before `ping` exits regardless of how many packets have been sent.
+    deadline: Option<Duration>,
+    /// The time to wait for a response for each packet.
+    ///
+    /// If `None`, TODO
+    timeout: Option<Duration>,
+    /// The size of packets to be sent.
+    packet_size: Option<usize>,
+    /// IP Time To Live.
+    ///
+    /// If `None`, TODO
+    ttl: Option<u32>,
 
-	/// The destination address or hostname.
-	dest: String,
+    /// The destination address or hostname.
+    dest: String,
 }
 
 impl Default for Args {
-	fn default() -> Self {
-		Self {
-			count: None,
-			interval: None,
-			deadline: None,
-			timeout: None,
-			packet_size: None,
-			ttl: None,
+    fn default() -> Self {
+        Self {
+            count: None,
+            interval: None,
+            deadline: None,
+            timeout: None,
+            packet_size: None,
+            ttl: None,
 
-			dest: String::new(),
-		}
-	}
+            dest: String::new(),
+        }
+    }
 }
 
 /// Parses command line arguments.
 fn parse_args() -> Args {
-	let mut iter = env::args();
-	let _bin = iter.next().unwrap_or_else(|| "ping".to_owned());
+    let mut iter = env::args();
+    let _bin = iter.next().unwrap_or_else(|| "ping".to_owned());
 
-	let mut args = Args::default();
+    let mut args = Args::default();
 
-	// TODO parse options
+    // TODO parse options
 
-	let Some(dest) = iter.next() else {
+    let Some(dest) = iter.next() else {
 		// TODO print usage
 		todo!();
 	};
-	args.dest = dest.parse().unwrap(); // TODO handle error
+    args.dest = dest.parse().unwrap(); // TODO handle error
 
-	args
+    args
 }
 
 fn main() {
-	let args = parse_args();
+    let args = parse_args();
 
-	let ctx = Arc::new(PingContext {
-		count: args.count,
-		interval: args.interval.unwrap_or(Duration::from_secs(1)),
-		deadline: args.deadline,
-		timeout: args.timeout.unwrap_or(Duration::from_secs(2)),
-		packet_size: args.packet_size.unwrap_or(56),
-		ttl: args.ttl.unwrap_or(115),
+    let ctx = Arc::new(PingContext {
+        count: args.count,
+        interval: args.interval.unwrap_or(Duration::from_secs(1)),
+        deadline: args.deadline,
+        timeout: args.timeout.unwrap_or(Duration::from_secs(2)),
+        packet_size: args.packet_size.unwrap_or(56),
+        ttl: args.ttl.unwrap_or(115),
 
-		dest: args.dest,
-		seq: Mutex::new(0),
-		int: Mutex::new(false),
-	});
+        dest: args.dest,
+        seq: Mutex::new(0),
+        int: Mutex::new(false),
+    });
     ctx.ping();
 }
--- ping/src/ping.rs
+++ ping/src/ping.rs
@@ -11,103 +11,108 @@ use std::time::Instant;
 
 /// A pinging context.
 pub struct PingContext {
-	/// The number of packets to send.
-	///
-	/// If `None`, there is no limit.
-	pub count: Option<NonZeroUsize>,
-	/// The interval between echo packets.
-	pub interval: Duration,
-	/// The timeout before `ping` exits regardless of how many packets have been sent.
-	///
-	/// If `None`, there is no deadline.
-	pub deadline: Option<Duration>,
-	/// The time to wait for a response for each packet.
-	pub timeout: Duration,
-	/// The size of packets to be sent.
-	pub packet_size: usize,
-	/// IP Time To Live.
-	pub ttl: u32,
+    /// The number of packets to send.
+    ///
+    /// If `None`, there is no limit.
+    pub count: Option<NonZeroUsize>,
+    /// The interval between echo packets.
+    pub interval: Duration,
+    /// The timeout before `ping` exits regardless of how many packets have been sent.
+    ///
+    /// If `None`, there is no deadline.
+    pub deadline: Option<Duration>,
+    /// The time to wait for a response for each packet.
+    pub timeout: Duration,
+    /// The size of packets to be sent.
+    pub packet_size: usize,
+    /// IP Time To Live.
+    pub ttl: u32,
 
-	/// The destination address or hostname.
-	pub dest: String,
-	/// The current sequence number.
-	pub seq: Mutex<usize>,
-	/// Tells whether pinging was interrupted.
-	pub int: Mutex<bool>,
+    /// The destination address or hostname.
+    pub dest: String,
+    /// The current sequence number.
+    pub seq: Mutex<usize>,
+    /// Tells whether pinging was interrupted.
+    pub int: Mutex<bool>,
 }
 
 impl PingContext {
-	/// Pings using the current context.
-	///
-	/// The function returns when pinging is over.
-	pub fn ping(self: Arc<Self>) {
-		let addr = "TODO"; // TODO resolve dns
-		println!("PING {} ({}) {} data bytes", self.dest, addr, self.packet_size);
+    /// Pings using the current context.
+    ///
+    /// The function returns when pinging is over.
+    pub fn ping(self: Arc<Self>) {
+        let addr = "TODO"; // TODO resolve dns
+        println!(
+            "PING {} ({}) {} data bytes",
+            self.dest, addr, self.packet_size
+        );
 
-		let start = Instant::now();
+        let start = Instant::now();
 
-		let self1 = self.clone();
-		thread::spawn(move || {
-			let mut signals = Signals::new(&[SIGINT]).unwrap();
-			if signals.forever().next().is_some() {
-				*self1.int.lock().unwrap() = true;
-			}
-		});
+        let self1 = self.clone();
+        thread::spawn(move || {
+            let mut signals = Signals::new(&[SIGINT]).unwrap();
+            if signals.forever().next().is_some() {
+                *self1.int.lock().unwrap() = true;
+            }
+        });
 
-		// Sending packets
-		let self2 = self.clone();
-		thread::spawn(move || {
-			loop {
-				let cont = self2.count
-					.map(|c| *self2.seq.lock().unwrap() < c.get())
-					.unwrap_or(true);
-				if !cont {
-					break;
-				}
+        // Sending packets
+        let self2 = self.clone();
+        thread::spawn(move || {
+            loop {
+                let cont = self2
+                    .count
+                    .map(|c| *self2.seq.lock().unwrap() < c.get())
+                    .unwrap_or(true);
+                if !cont {
+                    break;
+                }
 
-				// TODO send a packet
+                // TODO send a packet
 
-				*self2.seq.lock().unwrap() += 1;
+                *self2.seq.lock().unwrap() += 1;
 
-				thread::sleep(self2.interval);
-			}
-		});
+                thread::sleep(self2.interval);
+            }
+        });
 
-		let mut receive_count = 0;
+        let mut receive_count = 0;
 
-		while !*self.int.lock().unwrap() {
-			// Break if count has been reached
-			let cont = self.count
-				.map(|c| receive_count < c.get())
-				.unwrap_or(true);
-			if !cont {
-				break;
-			}
+        while !*self.int.lock().unwrap() {
+            // Break if count has been reached
+            let cont = self.count.map(|c| receive_count < c.get()).unwrap_or(true);
+            if !cont {
+                break;
+            }
 
-			// TODO block receiving packets
-			// TODO on receive:
-			// println!("{} bytes from {} ({}): icmp_seq={} ttl={} time={}");
+            // TODO block receiving packets
+            // TODO on receive:
+            // println!("{} bytes from {} ({}): icmp_seq={} ttl={} time={}");
 
-			receive_count += 1;
-		}
+            receive_count += 1;
+        }
 
-		let elapsed = start.elapsed();
+        let elapsed = start.elapsed();
 
-		let transmit_count = *self.seq.lock().unwrap();
-		let loss_count = if receive_count <= transmit_count {
-			transmit_count - receive_count
-		} else {
-			0
-		};
-		let loss_percentage = loss_count * 100 / transmit_count;
+        let transmit_count = *self.seq.lock().unwrap();
+        let loss_count = if receive_count <= transmit_count {
+            transmit_count - receive_count
+        } else {
+            0
+        };
+        let loss_percentage = loss_count * 100 / transmit_count;
 
-		println!();
-		println!("--- {} ping statistics ---", self.dest);
-		println!(
-			"{} packets transmitted, {} received, {}% packet loss, time {}ms",
-			transmit_count, receive_count, loss_percentage, elapsed.as_millis()
-		);
-		// TODO end:
-		// println!("rtt min/avg/max/mdev = {}/{}/{}/{} ms");
-	}
+        println!();
+        println!("--- {} ping statistics ---", self.dest);
+        println!(
+            "{} packets transmitted, {} received, {}% packet loss, time {}ms",
+            transmit_count,
+            receive_count,
+            loss_percentage,
+            elapsed.as_millis()
+        );
+        // TODO end:
+        // println!("rtt min/avg/max/mdev = {}/{}/{}/{} ms");
+    }
 }
