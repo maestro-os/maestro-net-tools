7f3df8f6270b2418f8667c6800cc575141ead179
llenotre <llenotre@student.42.fr>
2023-08-14 13:56:19 +0200

    feat(netlink): add `route.rs`

--- netlink/src/lib.rs
+++ netlink/src/lib.rs
@@ -2,16 +2,15 @@
 //!
 //! The netlink interface can be accessed from userspace through a socket.
 
+pub mod route;
 pub mod util;
 
-use std::ffi::c_int;
-use std::ffi::c_uchar;
-use std::ffi::c_uint;
-use std::ffi::c_ushort;
+use std::ffi::*;
 use std::io;
 use std::marker::PhantomData;
-use std::mem::size_of;
-use std::num::NonZeroUsize;
+
+/// Netlink family: route
+const NETLINK_ROUTE: c_int = 0;
 
 /// Netlink message header.
 #[repr(C)]
@@ -26,26 +25,6 @@ struct NlMsgHdr {
 	nlmsg_seq: u32,
 	/// Sender port ID
 	nlmsg_pid: u32,
-}
-
-/// Interface info message header.
-#[repr(C)]
-struct IfInfoMsg {
-	/// TODO doc
-	ifi_family: c_uchar,
-	/// Device type
-	ifi_type: c_ushort,
-	/// Interface index
-	ifi_index: c_int,
-	/// Device flags
-	ifi_flags: c_uint,
-	/// TODO doc
-	ifi_change: c_uint,
-}
-
-/// TODO doc
-pub struct Link {
-	// TODO
 }
 
 /// A netlink socket.
@@ -73,52 +52,6 @@ impl Netlink {
 	pub unsafe fn send_to(&self, _buf: &[u8]) -> io::Result<()> {
 		// TODO
 		todo!()
-	}
-
-	fn get_link_impl(&self, index: Option<NonZeroUsize>) -> io::Result<NetlinkIter<'_, Link>> {
-		let index = index.map(NonZeroUsize::get).unwrap_or(0);
-		// TODO
-		let seq = 0;
-
-		let hdr = NlMsgHdr {
-			nlmsg_len: (size_of::<NlMsgHdr>() + size_of::<IfInfoMsg>()) as _,
-			nlmsg_type: libc::RTM_GETLINK,
-			nlmsg_flags: 0,
-			nlmsg_seq: seq,
-			nlmsg_pid: 0,
-		};
-		let if_hdr = IfInfoMsg {
-			ifi_family: libc::AF_UNSPEC as _,
-			ifi_type: 0, // TODO
-			ifi_index: index as _,
-			ifi_flags: 0, // TODO
-			ifi_change: !0,
-		};
-
-		let mut buf = vec![];
-		buf.extend_from_slice(util::to_bytes(&hdr));
-		buf.extend_from_slice(util::to_bytes(&if_hdr));
-
-		unsafe { self.send_to(&buf) }?;
-
-		Ok(NetlinkIter {
-			sock: self,
-			seq,
-
-			_phantom: PhantomData,
-		})
-	}
-
-	/// Lists network interfaces.
-	pub fn list_links(&self) -> io::Result<NetlinkIter<'_, Link>> {
-		self.get_link_impl(None)
-	}
-
-	/// Returns the network interface with the given index.
-	///
-	/// If the interface doesn't exist, the function returns `None`.
-	pub fn get_link(&self, index: NonZeroUsize) -> io::Result<Option<Link>> {
-		self.get_link_impl(Some(index))?.next().transpose()
 	}
 }
 
---
+++ netlink/src/route.rs
@@ -0,0 +1,95 @@
+//! According to the Linux documentation, the route family: receives routing and link updates and
+//! may be used to modify the routing tables (both IPv4 and IPv6), IP addresses, link parameters,
+//! neighbor setups, queueing disciplines, traffic classes, and packet classifiers.
+
+use super::util;
+use super::Netlink;
+use super::NetlinkIter;
+use super::NlMsgHdr;
+use super::NETLINK_ROUTE;
+use std::ffi::*;
+use std::io;
+use std::marker::PhantomData;
+use std::mem::size_of;
+use std::num::NonZeroUsize;
+
+/// Interface info message header.
+#[repr(C)]
+struct IfInfoMsg {
+	/// TODO doc
+	ifi_family: c_uchar,
+	/// Device type
+	ifi_type: c_ushort,
+	/// Interface index
+	ifi_index: c_int,
+	/// Device flags
+	ifi_flags: c_uint,
+	/// TODO doc
+	ifi_change: c_uint,
+}
+
+/// TODO doc
+pub struct Link {
+	// TODO
+}
+
+/// A netlink socket set to the `NETLINK_ROUTE` family.
+pub struct RouteNetlink {
+	/// The netlink socket.
+	sock: Netlink,
+}
+
+impl RouteNetlink {
+	/// Creates a new instance.
+	pub fn new() -> io::Result<Self> {
+		Ok(Self {
+			sock: Netlink::new(NETLINK_ROUTE)?,
+		})
+	}
+
+	fn get_link_impl(&self, index: Option<NonZeroUsize>) -> io::Result<NetlinkIter<'_, Link>> {
+		let index = index.map(NonZeroUsize::get).unwrap_or(0);
+		// TODO
+		let seq = 0;
+
+		let hdr = NlMsgHdr {
+			nlmsg_len: (size_of::<NlMsgHdr>() + size_of::<IfInfoMsg>()) as _,
+			nlmsg_type: libc::RTM_GETLINK,
+			nlmsg_flags: 0,
+			nlmsg_seq: seq,
+			nlmsg_pid: 0,
+		};
+		let if_hdr = IfInfoMsg {
+			ifi_family: libc::AF_UNSPEC as _,
+			ifi_type: 0, // TODO
+			ifi_index: index as _,
+			ifi_flags: 0,
+			ifi_change: !0,
+		};
+
+		let mut buf = vec![];
+		buf.extend_from_slice(util::to_bytes(&hdr));
+		buf.extend_from_slice(util::to_bytes(&if_hdr));
+
+		unsafe { self.sock.send_to(&buf) }?;
+
+		Ok(NetlinkIter {
+			sock: &self.sock,
+			seq,
+
+			_phantom: PhantomData,
+		})
+	}
+
+	/// Lists network interfaces.
+	pub fn list_links(&self) -> io::Result<NetlinkIter<'_, Link>> {
+		self.get_link_impl(None)
+	}
+
+	/// Returns the network interface with the given index.
+	///
+	/// If the interface doesn't exist, the function returns `None`.
+	pub fn get_link(&self, index: NonZeroUsize) -> io::Result<Option<Link>> {
+		self.get_link_impl(Some(index))?.next().transpose()
+	}
+}
